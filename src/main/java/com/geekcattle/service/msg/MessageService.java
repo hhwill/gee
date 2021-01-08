6cdb79803e0c5042842f45806edb69dfb15b87c0
黄浩 <hao.huang@gingkoo.com>
2021-01-08 20:24:26 +0800

    bug fix bank

--- src/main/java/com/geekcattle/service/msg/MessageService.java
+++ src/main/java/com/geekcattle/service/msg/MessageService.java
@@ -115,16 +115,8 @@ public class MessageService {
                 } catch (Exception ex) {}
                 balance =
                         new DecimalFormat("0.00").format(Float.valueOf(balance) + curr1);
-                Bank _bank = new Bank();
-                _bank.setBankname(bankname);
-                _bank.setBalance(balance);
-                lbank.add(_bank);
                 bank.put(bankname,balance);
             } else {
-                Bank _bank = new Bank();
-                _bank.setBankname(bankname);
-                _bank.setBalance(lastvalue.get("BALANCE"));
-                lbank.add(_bank);
                 bank.put(bankname, lastvalue.get("BALANCE"));
             }
             Account _acc = new Account();
@@ -132,6 +124,12 @@ public class MessageService {
             _acc.setBankname(bankname);
             _acc.setBalance(lastvalue.get("BALANCE"));
             lacc.add(_acc);
+        }
+        for (String sbank : bank.keySet()) {
+            Bank _bank = new Bank();
+            _bank.setBankname(sbank);
+            _bank.setBalance(bank.get(sbank));
+            lbank.add(_bank);
         }
         result.setBalance(totalBalance);
         result.setUbalance(new DecimalFormat("0.00").format(Float.valueOf(totalBalance)/6.5));
@@ -195,11 +193,15 @@ public class MessageService {
         System.out.println(ser.getInfo());
         String s1 = "贵公司尾号1302的账户9月15日16时59分ARAP对账系统2003-2008服务费收入人民币11100.00元，余额27597.33元。对方户名:莎莎化妆品(中国）有限公司。[建设银行]";
         s1 = "贵公司尾号1302的账户9月15日16时59分ARAP对账系统2003-2008服务费收入人民币11100.00元，余额27597.33元。对方户名:莎莎化妆品(中国）有限公司。[建设银行]";
+        String s2 = "贵公司尾号1303的账户9月15日16时59分ARAP对账系统2003-2008服务费收入人民币11100.00元，余额27597.33元。对方户名:莎莎化妆品(中国）有限公司。[建设银行]";
         MsgUtil util = new MsgUtil();
-        Map<String,String> map = util.prase(s1);
-        List<Map<String,String>> lst = new ArrayList<Map<String,String>>();
-        lst.add(map);
-        accounts.put("1302", lst);
+        Map<String,String> map = new HashMap<String,String>();
+        map.put("msg", s1);
+        map.put("time", "1610105768");
+        ser.processMsg(map);
+        map.put("msg", s2);
+        map.put("time", "1610105769");
+        ser.processMsg(map);
 
         System.out.println(ser.getInfo());
         System.out.println(ser.getDetail("1302"));
